18fbc7a51c8bf4fc5b62e8ad412f085d31bcf51e
CyronF <cyronpfraser@gmail.com>
2021-08-24 21:19:13 -0400

    troubleshooting transaction subtraction

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = "static-cache";
+const DATA_CACHE_NAME = "data-cache";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
@@ -12,15 +12,17 @@ const FILES_TO_CACHE = [
   "icons/icon-512x512.png"
 ];
 
-// install
-self.addEventListener("install", function (evt) {
 
+self.addEventListener("install", function (evt) {
+  
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.add("/api/transaction"))
-  );
+  );  
+  
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
+
   self.skipWaiting();
 });
 
@@ -46,15 +48,14 @@ self.addEventListener("fetch", function(evt) {
     evt.respondWith(
       caches.open(CACHE_NAME).then(cache => {
         return fetch(evt.request)
-          .then(response => {
-            
+          .then(response => {            
             if (response.status === 200) {
               cache.put(evt.request.url, response.clone());
             }
 
             return response;
           })
-          .catch(err => {
+          .catch(err => {          
             return cache.match(evt.request);
           });
       }).catch(err => console.log(err))
