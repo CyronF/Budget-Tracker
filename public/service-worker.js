6f9cfaf96a505de044718a16158ca13a619cdf69
CyronF <cyronpfraser@gmail.com>
2021-08-25 20:59:04 -0400

    troubleshooting

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "static-cache";
-const DATA_CACHE_NAME = "data-cache";
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
@@ -12,20 +12,24 @@ const FILES_TO_CACHE = [
   "icons/icon-512x512.png"
 ];
 
-
+// install
 self.addEventListener("install", function (evt) {
-  
+  // pre cache image data
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.add("/api/transaction"))
-  );  
-  
+  );
+    
+  // pre cache all static assets
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 
+  // tell the browser to activate this service worker immediately once it
+  // has finished installing
   self.skipWaiting();
 });
 
+// activate
 self.addEventListener("activate", function(evt) {
   evt.waitUntil(
     caches.keys().then(keyList => {
@@ -43,19 +47,22 @@ self.addEventListener("activate", function(evt) {
   self.clients.claim();
 });
 
+// fetch
 self.addEventListener("fetch", function(evt) {
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
       caches.open(CACHE_NAME).then(cache => {
         return fetch(evt.request)
-          .then(response => {            
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
               cache.put(evt.request.url, response.clone());
             }
 
             return response;
           })
-          .catch(err => {          
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
             return cache.match(evt.request);
           });
       }).catch(err => console.log(err))
