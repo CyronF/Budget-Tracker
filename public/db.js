fa6f8a0be4b3b0ec3bfe4d01440d6abdc49381a1
CyronF <cyronpfraser@gmail.com>
2021-08-27 15:33:07 -0400

    edited checkDatabase function

--- public/db.js
+++ public/db.js
@@ -33,7 +33,7 @@ function saveRecord(record) {
 }
 
 function checkDatabase() {
-  const db = request.results;
+  const db = request.result;
   const transaction = db.transaction(["BudgetStore"], "readwrite");
   const budgetStore = transaction.objectStore("BudgetStore");
   const getAll = budgetStore.getAll();
